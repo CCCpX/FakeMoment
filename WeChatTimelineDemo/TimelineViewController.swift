0a1cf1ac5dbaa649c209cdafb06b744f8b032bba
peixiangchen <pxchencoder@gmail.com>
2017-08-15 17:09:24 +0800

    first commit

---
+++ WeChatTimelineDemo/TimelineViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TimelineViewController.swift
+//  WeChatTimelineDemo
+//
+//  Created by CPX on 15/08/2017.
+//  Copyright © 2017 CPX. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TimelineViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ WeChatTimelineDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WeChatTimelineDemo
+//
+//  Created by CPX on 15/08/2017.
+//  Copyright © 2017 CPX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func viewWeChatTimeline(_ sender: UIButton) {
+        
+    }
+    
+}
